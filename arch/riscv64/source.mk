4cde8bceb1ff14896884c224850f6c7611551175
Kimplul <kimi.h.kuparinen@gmail.com>
2023-05-12 00:36:18 +0300

    fix LLVM compilation
    + RELEASE=1 doesn't work for some reason, though

--- arch/riscv64/source.mk
+++ arch/riscv64/source.mk
@@ -4,16 +4,6 @@ INIT_SOURCES	+= $(ARCH_SOURCE)/init/*.[cS]
 
 # this doesn't work for rv32, but fine for now */
 ARCH_CFLAGS	:= -mcmodel=medany
-ARCH_LDFLAGS	:= -fuse-ld=bfd
-
-# slightly hacky but bfd is currently the only compiler that correctly handles
-# riscv relocations, but it doesn't recognise LLVM lto files, so if we're
-# doing an LLVM compilation, remove lto support
-DEBUGFLAGS	!= [ $(LLVM) ] && echo $(DEBUGFLAGS:-flto=) || echo $(DEBUGFLAGS)
-
-# llvm-objcopy is buggy, use gnu tools instead
-# christ, not much LLVM tooling available for RISCV :D
-OBJCOPY		:= $(CROSS_COMPILE)objcopy
 
 run:
 	$(ARCH_SOURCE)/conf/mkimage.sh $(ARCH)
