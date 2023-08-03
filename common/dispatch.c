4829e11dabc681e8901fa5e148129cd0c7116342
Kimplul <kimi.h.kuparinen@gmail.com>
2023-08-03 18:40:19 +0300

    dispatch doesn't have to worry about ipi

--- common/dispatch.c
+++ common/dispatch.c
@@ -31,11 +31,5 @@
 void dispatch(sys_arg_t a, sys_arg_t b, sys_arg_t c,
               sys_arg_t d, sys_arg_t e, sys_arg_t f)
 {
-	struct tcb *t = cur_tcb();
-	if (clear_ipi(t)) {
-		handle_ipi(t);
-		return;
-	}
-
-	handle_syscall(a, b, c, d, e, f, t);
+	handle_syscall(a, b, c, d, e, f, cur_tcb());
 }
