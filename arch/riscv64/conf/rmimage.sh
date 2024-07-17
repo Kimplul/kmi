83f8e82c421fa9013ac813b1560abf1c749822ca
Kimplul <kimi.h.kuparinen@gmail.com>
2024-07-17 19:34:03 +0300

    cleanup riscv64/conf

--- arch/riscv64/conf/rmimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e fs ]; then
-	rm -r fs
-fi
-
-if [ -e rootfs.img ]; then
-	rm rootfs.img
-fi
--- arch/riscv64/source.mk
+++ arch/riscv64/source.mk
@@ -4,13 +4,7 @@ KERNEL_SOURCES	:= $(KERNEL_SOURCES) $(KERNEL_LOCAL)
 # this doesn't work for rv32, but fine for now */
 ARCH_CFLAGS	:= $(ARCH_CFLAGS) -mcmodel=medany
 
-run:
-	$(ARCH_SOURCE)/conf/mkimage.sh $(ARCH)
-
 include $(ARCH_SOURCE)/asm/source.mk
 
 # dependecy generation
 $(ARCH_KERN_BUILD)/kernel/entry.o: $(ARCH_SOURCE)/kernel/gen/asm-offsets.h
-
-# full cleanup
-CLEANUP_CMD	:= $(ARCH_SOURCE)/conf/rmimage.sh
